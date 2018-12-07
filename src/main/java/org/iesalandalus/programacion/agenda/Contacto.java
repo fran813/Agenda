1e2ac41f59a085dae72f7d0de7cb3eddb0cc5850
Casa <Casa@Casa-PC>
2018-12-07 13:48:35 +0100

    Creados los atributos de la clase Contacto

---
+++ src/main/java/org/iesalandalus/programacion/agenda/Contacto.java
@@ -0,0 +1,14 @@
+package org.iesalandalus.programacion.agenda;
+
+import java.util.Objects;
+
+public class Contacto {
+	
+	private static final String ER_TELEFONO;
+	private static final String ER_CORREO;
+	
+	private String nombre;
+	private String telefono;
+	private String correo;
+
+}
