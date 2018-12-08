9a2716b28af34dc0b9872e52425d3f560b42184f
Casa <Casa@Casa-PC.Home>
2018-12-08 23:02:54 +0100

    Creado el constructor y los métodos get del diagrama

--- src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
+++ src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
@@ -2,5 +2,25 @@ package org.iesalandalus.programacion.agenda;
 
 public class Agenda {
 
-	private static final int MAX_CONTACTOS;
-}
+	private static final int MAX_CONTACTOS = 100;
+	
+		private int numContactos;
+		
+		public Agenda() {
+			
+			this.numContactos = numContactos;
+		}
+		
+		public Contacto[]getContactos() {
+			
+			Contacto arrayContactos [] = new Contacto[getNumContactos()];
+			Contacto [] contactos = new Contacto[getNumContactos()];
+			
+			System.arraycopy(arrayContactos, 0, contactos, 0, getNumContactos());
+			return contactos;
+			
+		}
+		public int getNumContactos() {
+			return numContactos;
+		}
+}	
